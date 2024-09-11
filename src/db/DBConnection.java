b3b9f54b80c600817b13bed408ea49ea594b4731
ThamiduChamod <thamiduchamod748@gmail.com>
2024-09-11 11:16:18 +0530

    config DBConnection

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -4,10 +4,32 @@
  */
 package db;
 
+import com.sun.jdi.connect.spi.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author a
  */
 public class DBConnection {
+    private static DBConnection dBConnection;
+    private Connection connection;
+            
+    private DBConnection() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "Thamidu1234");
+    }
     
+    public static DBConnection getInstance() throws ClassNotFoundException, SQLException{
+        if(dBConnection == null){
+            dBConnection = new DBConnection();
+        }
+        
+        return dBConnection;
+    }
+    
+   public Connection getConnection(){
+        return connection;
+    }
 }
